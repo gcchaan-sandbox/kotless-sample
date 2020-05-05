77523b051a38ce942de7733f6b730888c86d6682
gcchaan <gcchaan@users.noreply.github.com>
2020-05-05 21:54:11 +0900

    dynamodb-local for test

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     jcenter()
+    maven(
+        url = "https://s3-ap-northeast-1.amazonaws.com/dynamodb-local-tokyo/release"
+    )
 }
 
 dependencies {
@@ -20,8 +23,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
     implementation("io.kotless", "ktor-lang", "0.1.3")
     implementation("io.github.config4k", "config4k", "0.4.1")
+    implementation("com.amazonaws", "aws-java-sdk-dynamodb", "1.11.774")
     testImplementation("org.jetbrains.kotlin", "kotlin-test", "1.3.72")
     testImplementation("org.spekframework.spek2", "spek-dsl-jvm", "2.0.10")
+    testImplementation("com.amazonaws", "DynamoDBLocal", "1.12.0")
+    testImplementation("com.almworks.sqlite4java", "sqlite4java", "1.0.392")
     testRuntimeOnly("org.spekframework.spek2", "spek-runner-junit5", "2.0.10")
     testRuntimeOnly("org.jetbrains.kotlin", "kotlin-reflect", "1.3.72")
 }
--- src/test/kotlin/io/kotless/examples/controllers/TaskControllerSpec.kt
+++ src/test/kotlin/io/kotless/examples/controllers/TaskControllerSpec.kt
@@ -1,5 +1,8 @@
 package io.kotless.examples.controllers
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.local.embedded.DynamoDBEmbedded
+import com.amazonaws.services.dynamodbv2.model.* // ktlint-disable no-wildcard-imports
 import kotlin.test.assertEquals
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -9,6 +12,39 @@ object TaskControllerSpec : Spek({
         it("return all tasks") {
             val tc = TaskController()
             assertEquals(tc.findAll(), "[{\"id\":\"1\",\"title\":\"title\",\"description\":\"desc\"},{\"id\":\"2\",\"title\":\"title2\",\"description\":\"desc2\"}]")
+        }
+    }
+    describe("Task controller findAll") {
+        val util = AwsDynamoDbLocalTestUtils()
+        util.initSqLite()
+        lateinit var ddb: AmazonDynamoDB
+        beforeGroup {
+            ddb = DynamoDBEmbedded.create().amazonDynamoDB()
+        }
+        it("dynamodb local") {
+            val tableName = "kotlin_tasks"
+            val hashKeyName = "item_id"
+            val rcu = 100L
+            val wcu = 100L
+            val res = ddb.createTable(CreateTableRequest()
+                .withTableName(tableName)
+                .withKeySchema(listOf(
+                    KeySchemaElement(hashKeyName, KeyType.HASH)
+                ))
+                .withAttributeDefinitions(listOf(
+                    AttributeDefinition(hashKeyName, ScalarAttributeType.S)
+                ))
+                .withProvisionedThroughput(
+                    ProvisionedThroughput(rcu, wcu)
+                )
+            )
+            val tableDesc = res.tableDescription
+            assertEquals(tableName, tableDesc.tableName)
+            val tables = ddb.listTables()
+            assertEquals(1, tables.tableNames.size)
+        }
+        afterGroup {
+            ddb.shutdown()
         }
     }
 })
