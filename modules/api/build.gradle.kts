178d8474160724c7181f03364a454b84ea85c5ed
gcchaan <gcchaan@users.noreply.github.com>
2020-05-10 22:30:09 +0900

    buildSrc

--- modules/api/build.gradle.kts
+++ modules/api/build.gradle.kts
@@ -2,7 +2,7 @@ import io.kotless.plugin.gradle.dsl.kotless
 
 plugins {
     kotlin("jvm")
-    kotlin("plugin.serialization") version "1.3.72"
+    kotlin("plugin.serialization") version Versions.kotlin
     id("io.kotless") version "0.1.3" apply true
 }
 
@@ -18,12 +18,12 @@ dependencies {
     implementation("io.kotless", "ktor-lang", "0.1.3")
     implementation("io.github.config4k", "config4k", "0.4.1")
     implementation("com.amazonaws", "aws-java-sdk-dynamodb", "1.11.774")
-    testImplementation("org.jetbrains.kotlin", "kotlin-test", "1.3.72")
+    testImplementation("org.jetbrains.kotlin", "kotlin-test", Versions.kotlin)
     testImplementation("org.spekframework.spek2", "spek-dsl-jvm", "2.0.10")
     testImplementation("com.amazonaws", "DynamoDBLocal", "1.12.0")
     testImplementation("com.almworks.sqlite4java", "sqlite4java", "1.0.392")
     testRuntimeOnly("org.spekframework.spek2", "spek-runner-junit5", "2.0.10")
-    testRuntimeOnly("org.jetbrains.kotlin", "kotlin-reflect", "1.3.72")
+    testRuntimeOnly("org.jetbrains.kotlin", "kotlin-reflect", Versions.kotlin)
 }
 
 tasks {
--- modules/reminder/build.gradle.kts
+++ modules/reminder/build.gradle.kts
@@ -7,10 +7,10 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.amazonaws", "aws-lambda-java-core", "1.2.1")
     implementation("com.amazonaws", "aws-lambda-java-events", "2.2.8")
-    testImplementation("org.jetbrains.kotlin", "kotlin-test", "1.3.72")
+    testImplementation("org.jetbrains.kotlin", "kotlin-test", Versions.kotlin)
     testImplementation("org.spekframework.spek2", "spek-dsl-jvm", "2.0.10")
     testRuntimeOnly("org.spekframework.spek2", "spek-runner-junit5", "2.0.10")
-    testRuntimeOnly("org.jetbrains.kotlin", "kotlin-reflect", "1.3.72")
+    testRuntimeOnly("org.jetbrains.kotlin", "kotlin-reflect", Versions.kotlin)
 }
 
 tasks {
